730f3f6730e070319ddc3d8b1ce46318006f3f7a
lesleslie <les@wedgwoodwebworks.com>
2024-08-25 03:32:22 -0700

    ability to pass redis client

--- jinja2_async_environment/bccache.py
+++ jinja2_async_environment/bccache.py
@@ -2,29 +2,35 @@ import typing as t
 
 from jinja2 import BytecodeCache
 from jinja2.bccache import Bucket
-from redis.asyncio import Redis as AsyncRedis
+from redis.asyncio import Redis, RedisCluster
 from .environment import AsyncEnvironment
 
 
-class AsyncRedisBytecodeCache(AsyncRedis, BytecodeCache):  # type: ignore
+class AsyncRedisBytecodeCache(BytecodeCache):  # type: ignore
     def __init__(
         self,
         prefix: t.Optional[str] = None,
+        client: t.Optional[Redis | RedisCluster] = None,
         **configs: t.Any,
     ) -> None:
         super().__init__(**configs)
         self.prefix = prefix
+        self.client = client
+        if not client:
+            self.client = Redis(**configs)
 
     def get_bucket_name(self, key: str) -> str:
         return ":".join([self.prefix, key]) if self.prefix else key
 
     async def load_bytecode(self, bucket: Bucket) -> t.Any:  # type: ignore
-        code = await self.get(self.get_bucket_name(bucket.key))
+        code = await self.client.get(self.get_bucket_name(bucket.key))
         if code:
             return bucket.bytecode_from_string(code)
 
     async def dump_bytecode(self, bucket: Bucket) -> None:  # type: ignore
-        await self.set(self.get_bucket_name(bucket.key), bucket.bytecode_to_string())
+        await self.client.set(
+            self.get_bucket_name(bucket.key), bucket.bytecode_to_string()
+        )
 
     async def get_bucket(  # type: ignore
         self,
